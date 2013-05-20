b79bf1bfd868e92c74e6cf021aaa5760be624a28
Jane <fguanjun1004@gmail.com>
2013-05-20 14:35:29 +0800

    添加继发性诊断和原发性诊断的判断

--- HeadacheCDSSWeb/HeadacheCDSSWeb/Models/HeadacheDiagnosis.cs
+++ HeadacheCDSSWeb/HeadacheCDSSWeb/Models/HeadacheDiagnosis.cs
@@ -8,8 +8,17 @@ namespace HeadacheCDSSWeb.Models
     public class HeadacheDiagnosis
     {
         public List<string> GetDiagnosis(VisitData vData)
-        {
+        {  
             List<string> result = new List<string>();
+            if (vData.visitrecord.SecondaryHeadacheSymptom.Count!=0)
+           {
+                //继发性头痛
+            }
+            else
+            {
+                //原发性头痛
+            }
+          
             return result;
         }
     }
