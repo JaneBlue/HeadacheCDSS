8960a496e13d8effd9f4c61f5a134b96dd799dd5
Jane <fguanjun1004@gmail.com>
2013-06-17 10:53:18 +0800

    兼容性修改

---
+++ HeadacheCDSSWeb/HeadacheCDSSWeb/obj/Debug/Package/PackageTmp/Scripts/Diagnosis/ReporterOperation.js
@@ -0,0 +1,40 @@
+﻿ $(function () {  
+        var OCX = document.getElementById('Reporter2');
+        try {
+            OCX.OpenReport("D:\\CDSSOCX");
+        }
+        catch (err) {
+            var b = confirm("是否下载显示控件");
+            if (b) {
+                var strOCXURL;
+            strOCXURL= "http://"+location.host+"/LNCDCDSS/CDSSOCX.exe";
+            
+            window.location.href = strOCXURL;
+            }
+    }
+  
+  
+    
+   
+        OCX.ShowReport();
+        OCX.ShowThisPage("基本筛查");
+
+    $("#btn2").hide();
+    $("#btn2").click(function () {
+            $(".selected").last().removeClass("selected");
+            var name = $(".selected").last().attr("id");
+            $(".active").prev().addClass("active");
+            $(".active").last().removeClass("active");
+            OCX.ShowReport();
+            OCX.ShowThisPage(name);
+
+            if (name == "基本筛查") {
+                $("#btn2").hide();
+            }
+            if (name == "医嘱处置") {
+                $("#btn1").html("下一步");
+            }
+     });
+       
+
+});
